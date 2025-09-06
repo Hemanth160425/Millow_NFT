5b7c4472911a3b7ab8db4e934ebdd1562c4bbabe
Hemanth <hemanth.221ee231@nitk.edu.in>
2025-09-06 22:52:48 +0530

    mmm

--- src/App.js
+++ src/App.js
@@ -7,79 +7,114 @@ import Search from './components/Search';
 import Home from './components/Home';
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from './abis/RealEstate.json';
+import Escrow from './abis/Escrow.json';
 
 // Config
 import config from './config.json';
 
-
 function App() {
-  const [provider , setProvider] = useState(null);
-  const [account , setAccount] = useState(null);
-
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [homes, setHomes] = useState([]);
+  const [home, setHome] = useState({});
+  const [toggle, setToggle] = useState(false);
 
   const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);  
+    // Initialize provider
+    const provider = new ethers.BrowserProvider(window.ethereum);
     setProvider(provider);
+    
+    // Get network and signer
     const network = await provider.getNetwork();
+    const signer = await provider.getSigner();
+    
+    // Get account
+    const accounts = await provider.send("eth_requestAccounts", []);
+    const account = ethers.getAddress(accounts[0]);
+    setAccount(account);
 
-    console.log("Connected chainId:", network.chainId);
-    console.log("Config keys:", Object.keys(config));
-
+    // Initialize RealEstate contract
     const realEstate = new ethers.Contract(
-      config[network.chainId.toString()].realEstate.address,
-      RealEstate.abi, 
-      provider
-    );    
-    const totalsupply = await realEstate.totalSupply()
-    console.log(totalsupply.toString())
+      config[network.chainId].realEstate.address,
+      RealEstate.abi,
+      signer
+    );
 
-    
-    
-    window.ethereum.on('accountsChanged', async (newAccounts) => {
-      const newAccount = ethers.utils.getAddress(newAccounts[0]);
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const account = ethers.utils.getAddress(accounts[0]);
-      setAccount(account); 
-    });    
-  } 
+    // Load homes
+    const totalSupply = await realEstate.totalSupply();
+    const homes = [];
+
+    for (let i = 1; i <= totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+
+    setHomes(homes);
+
+    // Initialize Escrow contract
+    const escrow = new ethers.Contract(
+      config[network.chainId].escrow.address,
+      Escrow.abi,
+      signer
+    );
+    setEscrow(escrow);
+
+    // Handle account changes
+    window.ethereum.on('accountsChanged', async () => {
+      const accounts = await provider.send("eth_requestAccounts", []);
+      setAccount(ethers.getAddress(accounts[0]));
+    });
+  };
+
   useEffect(() => {
-    loadBlockchainData()
-  }, []); 
+    loadBlockchainData();
+  }, []);
 
+  const togglePop = (home) => {
+    setHome(home);
+    setToggle(!toggle);
+  };
 
   return (
     <div>
-      < Navigation account={account} setAccount={setAccount} />
+      <Navigation account={account} setAccount={setAccount} />
       <Search />
       <div className='cards__section'>
-
-        <h3>Homes for you</h3>
+        <h3>Homes For You</h3>
         <hr />
         <div className='cards'>
-          <div className='card'>
-            <div className='card__image'>
-              <img src="" alt="Home" />
+          {homes.map((home, index) => (
+            <div className='card' key={index} onClick={() => togglePop(home)}>
+              <div className='card__image'>
+                <img src={home.image} alt="Home" />
+              </div>
+              <div className='card__info'>
+                <h4>{home.attributes[0].value} ETH</h4>
+                <p>
+                  <strong>{home.attributes[2].value}</strong> bds |
+                  <strong>{home.attributes[3].value}</strong> ba |
+                  <strong>{home.attributes[4].value}</strong> sqft
+                </p>
+                <p>{home.address}</p>
+              </div>
             </div>
-            <div className='card__info'>
-              <h4>1 ETH</h4>
-              <p>
-                <strong>1</strong>bds |
-                <strong>2</strong>ba |
-                <strong>3</strong>sqft 
-              </p>
-              <p>
-                1234 Elm St
-              </p>
-            </div>
-            
-          </div>
-          
+          ))}
         </div>
- 
       </div>
 
+      {toggle && (
+        <Home 
+          home={home} 
+          provider={provider} 
+          account={account} 
+          escrow={escrow} 
+          togglePop={togglePop} 
+        />
+      )}
     </div>
   );
 }
